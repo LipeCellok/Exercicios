66eb873801dfe4379e541e5aade751e6137c8155
Felipe Espíndola <felipe.espindola@outlook.com.br>
2024-08-26 22:08:13 -0300

    Update

---
+++ Cronometro/Cronometro/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Menu();
+        Start(6);
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundos");
+        Console.WriteLine("M = Minutos");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto Tempo deseja Contar");
+
+    }
+    static void Start(int time)
+    {
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("Finalizado");
+        Thread.Sleep(2500);
+    }
+}
