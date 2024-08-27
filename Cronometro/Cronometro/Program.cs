23076a143eefcbf0bd8c6bf6d51714d17413380f
Felipe Espíndola <felipe.espindola@outlook.com.br>
2024-08-26 22:58:23 -0300

    Update

--- Cronometro/Cronometro/Program.cs
+++ Cronometro/Cronometro/Program.cs
@@ -2,8 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        //Menu();
-        Start(6);
+        Menu();
+        
     }
 
     static void Menu()
@@ -13,6 +13,20 @@
         Console.WriteLine("M = Minutos");
         Console.WriteLine("0 = Sair");
         Console.WriteLine("Quanto Tempo deseja Contar");
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length-1, 1));
+        int time = int.Parse(data.Substring(0, data.Length -1));
+        int multiplier = 1;
+
+        if (type == 'm')
+            multiplier = 60;
+
+        if (time == 0)
+            System.Environment.Exit(0);
+
+        PreStart(time * multiplier);
+     
 
     }
     static void Start(int time)
@@ -30,5 +44,19 @@
         Console.Clear();
         Console.WriteLine("Finalizado");
         Thread.Sleep(2500);
+        Menu();
+    }
+
+    static void PreStart(int time) 
+    {
+        Console.Clear();
+        Console.WriteLine("Ready...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Set...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Go...");
+        Thread.Sleep(2500);
+
+        Start(time);
     }
 }
