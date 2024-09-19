2119fa90f3989f624ccb2c1b33e95f0335080b77
Felipe Espíndola <felipe.espindola@outlook.com.br>
2024-09-19 20:35:07 -0300

    Update

--- EditorHtml/EditorHtml/Menu.cs
+++ EditorHtml/EditorHtml/Menu.cs
@@ -9,7 +9,10 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
-            DrawScreen();
+            DrawScreen(); 
+            WriteOptions(); 
+
+            var option = short.Parse(Console.ReadLine());          
 
         }
 
@@ -38,8 +41,26 @@ namespace EditorHtml
                 Console.Write("-");
 
             Console.Write("+");
-            Console.Write("\n");
+            Console.WriteLine("\n");
         }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo:");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção:");
+        }        
     }
 
 }
